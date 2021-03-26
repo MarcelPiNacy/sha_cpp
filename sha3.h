ca03094160cb4566280a0c5256e0c7e6dca79c5e
MarcelPiNacy <marcelpi97@gmail.com>
2021-03-26 19:02:59 +0100

    Removed license from sha3.h + moved some of the code to sha_common.h. Renamed sha3 namespace to sha3 and hasher to sha3_hasher.

--- sha3.h
+++ sha3.h
@@ -1,72 +1,8 @@
-/*
-	BSD 2-Clause License
-	
-	Copyright (c) 2021, Marcel Pi Nacy
-	All rights reserved.
-	
-	Redistribution and use in source and binary forms, with or without
-	modification, are permitted provided that the following conditions are met:
-	
-	1. Redistributions of source code must retain the above copyright notice, this
-	   list of conditions and the following disclaimer.
-	
-	2. Redistributions in binary form must reproduce the above copyright notice,
-	   this list of conditions and the following disclaimer in the documentation
-	   and/or other materials provided with the distribution.
-	
-	THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-	AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-	IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-	DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-	FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-	DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-	SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-	CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-	OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-	OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
-*/
-
 #pragma once
-#include <cstdint>
+#include "sha_common.h"
 
-#if defined(_DEBUG) || !defined(NDEBUG)
-#define SHA_CPP_DEBUG
-#include <cassert>
-#endif
-
-#ifdef _MSVC_LANG
-#include <intrin.h>
-#include <cstring>
-#define SHA_CPP_INLINE_ALWAYS __forceinline
-#define SHA_CPP_INLINE_NEVER __declspec(noinline)
-#ifdef SHA_CPP_BIG_ENDIAN
-#define SHA_CPP_BSWAP64(MASK) _byteswap_uint64((MASK))
-#endif
-#define SHA_CPP_ROTL64(MASK, AMOUNT) _rotl64((MASK), (AMOUNT))
-#else // assume CLang-like compiler
-#define SHA_CPP_INLINE_ALWAYS __attribute__((always_inline))
-#define SHA_CPP_INLINE_NEVER __attribute__((noinline))
-#ifdef SHA_CPP_BIG_ENDIAN
-#define SHA_CPP_BSWAP64(MASK) __bswap64((MASK))
-#endif
-#define SHA_CPP_ROTL64(MASK, AMOUNT) __builtin_rotateleft64((MASK), (AMOUNT))
-#endif
-
-#ifdef SHA_CPP_STRING_VIEW
-#include <string_view>
-#endif
-
-namespace sha3
+namespace sha_cpp
 {
-	static constexpr uint8_t HASH_STATE_SIZE = 25;
-	static constexpr uint8_t MAX_HASH_RATE = 24;
-
-	template <typename T, uint32_t Size>
-	using array_type = T[Size];
-
-	template <typename T, uint32_t Size>
-	using array_ref = array_type<T, Size>&;
-
 	constexpr uint8_t KECCAK_ROUND_COUNT = 24;
 	constexpr uint64_t KECCAK_ROUND_LOOKUP[KECCAK_ROUND_COUNT] =
 	{
@@ -78,116 +14,11 @@ namespace sha3
 		0x8000000080008081, 0x8000000000008080, 0x0000000080000001, 0x8000000080008008
 	};
 
-	namespace detail
+	namespace detail::sha3
 	{
-		constexpr char hex_lookup_upper[] =
-			"000102030405060708090A0B0C0D0E0F"
-			"101112131415161718191A1B1C1D1E1F"
-			"202122232425262728292A2B2C2D2E2F"
-			"303132333435363738393A3B3C3D3E3F"
-			"404142434445464748494A4B4C4D4E4F"
-			"505152535455565758595A5B5C5D5E5F"
-			"606162636465666768696A6B6C6D6E6F"
-			"707172737475767778797A7B7C7D7E7F"
-			"808182838485868788898A8B8C8D8E8F"
-			"909192939495969798999A9B9C9D9E9F"
-			"A0A1A2A3A4A5A6A7A8A9AAABACADAEAF"
-			"B0B1B2B3B4B5B6B7B8B9BABBBCBDBEBF"
-			"C0C1C2C3C4C5C6C7C8C9CACBCCCDCECF"
-			"D0D1D2D3D4D5D6D7D8D9DADBDCDDDEDF"
-			"E0E1E2E3E4E5E6E7E8E9EAEBECEDEEEF"
-			"F0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF";
-		constexpr char hex_lookup_lower[] =
-			"000102030405060708090a0b0c0d0e0f"
-			"101112131415161718191a1b1c1d1e1f"
-			"202122232425262728292a2b2c2d2e2f"
-			"303132333435363738393a3b3c3d3e3f"
-			"404142434445464748494a4b4c4d4e4f"
-			"505152535455565758595a5b5c5d5e5f"
-			"606162636465666768696a6b6c6d6e6f"
-			"707172737475767778797a7b7c7d7e7f"
-			"808182838485868788898a8b8c8d8e8f"
-			"909192939495969798999a9b9c9d9e9f"
-			"a0a1a2a3a4a5a6a7a8a9aaabacadaeaf"
-			"b0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
-			"c0c1c2c3c4c5c6c7c8c9cacbcccdcecf"
-			"d0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
-			"e0e1e2e3e4e5e6e7e8e9eaebecedeeef"
-			"f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
-	}
+		static constexpr uint8_t HASH_STATE_SIZE = 25;
+		static constexpr uint8_t MAX_HASH_RATE = 24;
 
-	template <uint32_t Bits>
-	struct hash
-	{
-		static constexpr uint32_t hash_bits = Bits;
-		static constexpr uint32_t hash_bytes = hash_bits / 8;
-		static constexpr uint32_t hash_qwords = hash_bytes / 8;
-
-		static constexpr uint32_t hex_digit_count = hash_bytes * 2;
-
-		uint64_t hash_data[hash_qwords];
-
-		template <bool LowerCase = true>
-		void to_hex(char* out)
-		{
-			// Johnny Lee's uint64_t to hex string:
-			// https://johnnylee-sde.github.io/Fast-unsigned-integer-to-hex-string/
-
-			constexpr auto& lookup = (LowerCase ? detail::hex_lookup_lower : detail::hex_lookup_upper);
-			for (uint_fast8_t mask_index = 0; mask_index != hash_qwords; ++mask_index)
-			{
-				uint_fast64_t mask = hash_data[mask_index];
-				uint_fast16_t lookup_index;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[15] = lookup[lookup_index + 1];
-				out[14] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[13] = lookup[lookup_index + 1];
-				out[12] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[11] = lookup[lookup_index + 1];
-				out[10] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[9] = lookup[lookup_index + 1];
-				out[8] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[7] = lookup[lookup_index + 1];
-				out[6] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[5] = lookup[lookup_index + 1];
-				out[4] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[3] = lookup[lookup_index + 1];
-				out[2] = lookup[lookup_index];
-				mask >>= 8;
-
-				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
-				out[1] = lookup[lookup_index + 1];
-				out[0] = lookup[lookup_index];
-				mask >>= 8;
-
-				out += 16;
-			}
-		}
-	};
-
-	
-
-	namespace detail
-	{
 		template <uint32_t Bits>
 		struct hash_state
 		{
@@ -360,63 +191,58 @@ namespace sha3
 			keccak_chi_step<20>(state);
 		}
 
-		SHA_CPP_INLINE_ALWAYS void sha3_permute(array_ref<uint64_t, HASH_STATE_SIZE> state) noexcept
-		{
-			for (uint8_t i = 0; i < KECCAK_ROUND_COUNT; i++)
-			{
-				keccak_theta(state);
-				state[1] = SHA_CPP_ROTL64(state[1], 1);
-				state[2] = SHA_CPP_ROTL64(state[2], 62);
-				state[3] = SHA_CPP_ROTL64(state[3], 28);
-				state[4] = SHA_CPP_ROTL64(state[4], 27);
-				state[5] = SHA_CPP_ROTL64(state[5], 36);
-				state[6] = SHA_CPP_ROTL64(state[6], 44);
-				state[7] = SHA_CPP_ROTL64(state[7], 6);
-				state[8] = SHA_CPP_ROTL64(state[8], 55);
-				state[9] = SHA_CPP_ROTL64(state[9], 20);
-				state[10] = SHA_CPP_ROTL64(state[10], 3);
-				state[11] = SHA_CPP_ROTL64(state[11], 10);
-				state[12] = SHA_CPP_ROTL64(state[12], 43);
-				state[13] = SHA_CPP_ROTL64(state[13], 25);
-				state[14] = SHA_CPP_ROTL64(state[14], 39);
-				state[15] = SHA_CPP_ROTL64(state[15], 41);
-				state[16] = SHA_CPP_ROTL64(state[16], 45);
-				state[17] = SHA_CPP_ROTL64(state[17], 15);
-				state[18] = SHA_CPP_ROTL64(state[18], 21);
-				state[19] = SHA_CPP_ROTL64(state[19], 8);
-				state[20] = SHA_CPP_ROTL64(state[20], 18);
-				state[21] = SHA_CPP_ROTL64(state[21], 2);
-				state[22] = SHA_CPP_ROTL64(state[22], 61);
-				state[23] = SHA_CPP_ROTL64(state[23], 56);
-				state[24] = SHA_CPP_ROTL64(state[24], 14);
-				keccak_pi(state);
-				keccak_chi(state);
-				state[0] ^= KECCAK_ROUND_LOOKUP[i];
-			}
-		}
-
 		template <uint8_t BlockSize>
-		SHA_CPP_INLINE_ALWAYS void sha3_process_block(array_ref<uint64_t, HASH_STATE_SIZE> hash, const uint64_t* block) noexcept
+		SHA_CPP_INLINE_ALWAYS void process_block(array_ref<uint64_t, HASH_STATE_SIZE> hash, const uint64_t* block) noexcept
 		{
-			constexpr uint8_t COUNT =
+			constexpr uint8_t Count =
 				BlockSize <= 72 ? 9 :
 				BlockSize <= 104 ? 13 :
 				BlockSize <= 136 ? 17 :
 				BlockSize <= 144 ? 18 : 25;
 
-			for (uint8_t i = 0; i != COUNT; ++i)
+			for (uint8_t i = 0; i != Count; ++i)
 				hash[i] ^= to_little_endian(block[i]);
 
-			sha3_permute(hash);
+			for (uint8_t i = 0; i < KECCAK_ROUND_COUNT; i++)
+			{
+				keccak_theta(hash);
+				hash[1]  = SHA_CPP_ROTL64(hash[1], 1);
+				hash[2]  = SHA_CPP_ROTL64(hash[2], 62);
+				hash[3]  = SHA_CPP_ROTL64(hash[3], 28);
+				hash[4]  = SHA_CPP_ROTL64(hash[4], 27);
+				hash[5]  = SHA_CPP_ROTL64(hash[5], 36);
+				hash[6]  = SHA_CPP_ROTL64(hash[6], 44);
+				hash[7]  = SHA_CPP_ROTL64(hash[7], 6);
+				hash[8]  = SHA_CPP_ROTL64(hash[8], 55);
+				hash[9]  = SHA_CPP_ROTL64(hash[9], 20);
+				hash[10] = SHA_CPP_ROTL64(hash[10], 3);
+				hash[11] = SHA_CPP_ROTL64(hash[11], 10);
+				hash[12] = SHA_CPP_ROTL64(hash[12], 43);
+				hash[13] = SHA_CPP_ROTL64(hash[13], 25);
+				hash[14] = SHA_CPP_ROTL64(hash[14], 39);
+				hash[15] = SHA_CPP_ROTL64(hash[15], 41);
+				hash[16] = SHA_CPP_ROTL64(hash[16], 45);
+				hash[17] = SHA_CPP_ROTL64(hash[17], 15);
+				hash[18] = SHA_CPP_ROTL64(hash[18], 21);
+				hash[19] = SHA_CPP_ROTL64(hash[19], 8);
+				hash[20] = SHA_CPP_ROTL64(hash[20], 18);
+				hash[21] = SHA_CPP_ROTL64(hash[21], 2);
+				hash[22] = SHA_CPP_ROTL64(hash[22], 61);
+				hash[23] = SHA_CPP_ROTL64(hash[23], 56);
+				hash[24] = SHA_CPP_ROTL64(hash[24], 14);
+				keccak_pi(hash);
+				keccak_chi(hash);
+				hash[0] ^= KECCAK_ROUND_LOOKUP[i];
+			}
 		}
 	}
 
 
 
 	template <uint32_t Bits>
-	struct hasher : detail::hash_state<Bits>
+	struct sha3_hasher : detail::sha3::hash_state<Bits>
 	{
-		using base = detail::hash_state<Bits>;
+		using base = detail::sha3::hash_state<Bits>;
 
 #ifdef SHA_CPP_DEBUG
 		bool debug_finalized_flag;
@@ -431,21 +257,21 @@ namespace sha3
 			uint8_t* ptr = (uint8_t*)data;
 			size_t index = base::message_length;
 			base::message_length += (uint8_t)size;
-			base::message_length = (uint8_t)detail::fast_mod<BLOCK_SIZE>(base::message_length);
+			base::message_length = (uint8_t)detail::sha3::fast_mod<BLOCK_SIZE>(base::message_length);
 			if (index != 0)
 			{
 				const size_t left = BLOCK_SIZE - index;
 				(void)memcpy((uint8_t*)base::message + index, ptr, size < left ? size : left);
 				if (size < left)
 					return;
-				detail::sha3_process_block<BLOCK_SIZE>(base::hash, base::message);
+				detail::sha3::process_block<BLOCK_SIZE>(base::hash, base::message);
 				ptr += left;
 				size -= left;
 			}
 			while (size >= BLOCK_SIZE)
 			{
 				uint64_t* aligned_message_block;
-				if (detail::is_8byte_aligned(ptr))
+				if (detail::sha3::is_8byte_aligned(ptr))
 				{
 					aligned_message_block = (uint64_t*)ptr;
 				}
@@ -454,7 +280,7 @@ namespace sha3
 					(void)memcpy(base::message, ptr, BLOCK_SIZE);
 					aligned_message_block = base::message;
 				}
-				detail::sha3_process_block<BLOCK_SIZE>(base::hash, aligned_message_block);
+				detail::sha3::process_block<BLOCK_SIZE>(base::hash, aligned_message_block);
 				ptr += BLOCK_SIZE;
 				size -= BLOCK_SIZE;
 			}
@@ -475,26 +301,26 @@ namespace sha3
 			size_t size = Size;
 			size_t index = base::message_length;
 			base::message_length += (uint8_t)size;
-			base::message_length = (uint8_t)detail::fast_mod<BLOCK_SIZE>(base::message_length);
+			base::message_length = (uint8_t)detail::sha3::fast_mod<BLOCK_SIZE>(base::message_length);
 			if (index != 0)
 			{
 				const size_t left = BLOCK_SIZE - index;
 				(void)memcpy((uint8_t*)base::message + index, ptr, size < left ? size : left);
 				if (size < left)
 					return;
-				detail::sha3_process_block<BLOCK_SIZE>(base::hash, base::message);
+				detail::sha3::process_block<BLOCK_SIZE>(base::hash, base::message);
 				ptr += left;
 				size -= left;
 			}
 			while (size >= BLOCK_SIZE)
 			{
 				uint64_t* aligned_message_block = (uint64_t*)ptr;
-				if (!detail::is_8byte_aligned(ptr))
+				if (!detail::sha3::is_8byte_aligned(ptr))
 				{
 					(void)memcpy(base::message, ptr, BLOCK_SIZE);
 					aligned_message_block = base::message;
 				}
-				detail::sha3_process_block<BLOCK_SIZE>(base::hash, aligned_message_block);
+				detail::sha3::process_block<BLOCK_SIZE>(base::hash, aligned_message_block);
 				ptr += BLOCK_SIZE;
 				size -= BLOCK_SIZE;
 			}
@@ -550,8 +376,8 @@ namespace sha3
 			constexpr uint64_t MASK = KeccakFinalizer ? 0x01 : 0x06;
 			((uint8_t*)base::message)[base::message_length] |= MASK;
 			((uint8_t*)base::message)[BLOCK_SIZE - 1] |= 0x80;
-			detail::sha3_process_block<BLOCK_SIZE>(base::hash, base::message);
-			detail::to_little_endian_copy(r.hash_data, base::hash, DIGEST_SIZE);
+			detail::sha3::process_block<BLOCK_SIZE>(base::hash, base::message);
+			detail::sha3::to_little_endian_copy(r.hash_data, base::hash, DIGEST_SIZE);
 #ifdef SHA_CPP_DEBUG
 			debug_finalized_flag = true;
 #endif
---
+++ sha_common.h
@@ -0,0 +1,145 @@
+#pragma once
+#include <cstdint>
+
+#if defined(_DEBUG) || !defined(NDEBUG)
+#define SHA_CPP_DEBUG
+#include <cassert>
+#endif
+
+#ifdef _MSVC_LANG
+#include <intrin.h>
+#include <cstring>
+#define SHA_CPP_INLINE_ALWAYS __forceinline
+#define SHA_CPP_INLINE_NEVER __declspec(noinline)
+#ifdef SHA_CPP_BIG_ENDIAN
+#define SHA_CPP_BSWAP64(MASK) _byteswap_uint64((MASK))
+#endif
+#define SHA_CPP_ROTL64(MASK, AMOUNT) _rotl64((MASK), (AMOUNT))
+#else // assume CLang-like compiler
+#define SHA_CPP_INLINE_ALWAYS __attribute__((always_inline))
+#define SHA_CPP_INLINE_NEVER __attribute__((noinline))
+#ifdef SHA_CPP_BIG_ENDIAN
+#define SHA_CPP_BSWAP64(MASK) __bswap64((MASK))
+#endif
+#define SHA_CPP_ROTL64(MASK, AMOUNT) __builtin_rotateleft64((MASK), (AMOUNT))
+#endif
+
+#ifdef SHA_CPP_STRING_VIEW
+#include <string_view>
+#endif
+
+namespace sha_cpp
+{
+	template <typename T, uint32_t Size>
+	using array_type = T[Size];
+
+	template <typename T, uint32_t Size>
+	using array_ref = array_type<T, Size>&;
+
+	namespace detail
+	{
+		constexpr char hex_lookup_upper[] =
+			"000102030405060708090A0B0C0D0E0F"
+			"101112131415161718191A1B1C1D1E1F"
+			"202122232425262728292A2B2C2D2E2F"
+			"303132333435363738393A3B3C3D3E3F"
+			"404142434445464748494A4B4C4D4E4F"
+			"505152535455565758595A5B5C5D5E5F"
+			"606162636465666768696A6B6C6D6E6F"
+			"707172737475767778797A7B7C7D7E7F"
+			"808182838485868788898A8B8C8D8E8F"
+			"909192939495969798999A9B9C9D9E9F"
+			"A0A1A2A3A4A5A6A7A8A9AAABACADAEAF"
+			"B0B1B2B3B4B5B6B7B8B9BABBBCBDBEBF"
+			"C0C1C2C3C4C5C6C7C8C9CACBCCCDCECF"
+			"D0D1D2D3D4D5D6D7D8D9DADBDCDDDEDF"
+			"E0E1E2E3E4E5E6E7E8E9EAEBECEDEEEF"
+			"F0F1F2F3F4F5F6F7F8F9FAFBFCFDFEFF";
+		constexpr char hex_lookup_lower[] =
+			"000102030405060708090a0b0c0d0e0f"
+			"101112131415161718191a1b1c1d1e1f"
+			"202122232425262728292a2b2c2d2e2f"
+			"303132333435363738393a3b3c3d3e3f"
+			"404142434445464748494a4b4c4d4e4f"
+			"505152535455565758595a5b5c5d5e5f"
+			"606162636465666768696a6b6c6d6e6f"
+			"707172737475767778797a7b7c7d7e7f"
+			"808182838485868788898a8b8c8d8e8f"
+			"909192939495969798999a9b9c9d9e9f"
+			"a0a1a2a3a4a5a6a7a8a9aaabacadaeaf"
+			"b0b1b2b3b4b5b6b7b8b9babbbcbdbebf"
+			"c0c1c2c3c4c5c6c7c8c9cacbcccdcecf"
+			"d0d1d2d3d4d5d6d7d8d9dadbdcdddedf"
+			"e0e1e2e3e4e5e6e7e8e9eaebecedeeef"
+			"f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff";
+	}
+
+	template <uint32_t Bits>
+	struct hash
+	{
+		static constexpr uint32_t hash_bits = Bits;
+		static constexpr uint32_t hash_bytes = hash_bits / 8;
+		static constexpr uint32_t hash_qwords = hash_bytes / 8;
+
+		static constexpr uint32_t hex_digit_count = hash_bytes * 2;
+
+		uint64_t hash_data[hash_qwords];
+
+		template <bool LowerCase = true>
+		void to_hex(char* out)
+		{
+			// Johnny Lee's uint64_t to hex string:
+			// https://johnnylee-sde.github.io/Fast-unsigned-integer-to-hex-string/
+
+			constexpr auto& lookup = (LowerCase ? detail::hex_lookup_lower : detail::hex_lookup_upper);
+			for (uint_fast8_t mask_index = 0; mask_index != hash_qwords; ++mask_index)
+			{
+				uint_fast64_t mask = hash_data[mask_index];
+				uint_fast16_t lookup_index;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[15] = lookup[lookup_index + 1];
+				out[14] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[13] = lookup[lookup_index + 1];
+				out[12] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[11] = lookup[lookup_index + 1];
+				out[10] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[9] = lookup[lookup_index + 1];
+				out[8] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[7] = lookup[lookup_index + 1];
+				out[6] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[5] = lookup[lookup_index + 1];
+				out[4] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[3] = lookup[lookup_index + 1];
+				out[2] = lookup[lookup_index];
+				mask >>= 8;
+
+				lookup_index = (uint_fast16_t)2 * (uint8_t)mask;
+				out[1] = lookup[lookup_index + 1];
+				out[0] = lookup[lookup_index];
+				mask >>= 8;
+
+				out += 16;
+			}
+		}
+	};
+
+}
